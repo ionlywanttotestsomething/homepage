b45c47ed7a825bb91a5bb066bddb903c471ae1fb
ionlywanttotestsomething <158832323+ionlywanttotestsomething@users.noreply.github.com>
2024-02-04 20:39:53 +0000

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent},
+  { path: '/homepage', component: HomeComponent},
   { path: '**', pathMatch: 'full',  
   component: PageNotFoundComponent }, 
 ];
