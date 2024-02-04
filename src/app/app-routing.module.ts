d871370341233a610cad40a8bc7018734b275289
ionlywanttotestsomething <158832323+ionlywanttotestsomething@users.noreply.github.com>
2024-02-04 22:53:20 +0000

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'form', component: AddressFormComponent},
   { path: '**', pathMatch: 'full',  
   component: PageNotFoundComponent }, 
 ];
